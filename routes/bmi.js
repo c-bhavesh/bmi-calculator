7b877bece0d74fdd5ab6bd126e1c23b934b4e752
Bhavesh Chauhan <bhavesh.chauhan2009@gmail.com>
2021-06-14 14:44:17 +0530

    bmi calculation added

--- routes/bmi.js
+++ routes/bmi.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-
-const users = [{name: 'Tony', email: 'tony@gmail.com'}]
-
 router.get('/', (_, res)=>{
     const message = [{ status : 'error', error: 'Body Mass Index can\'t calculate without height & weight.' }]
     res.json({ ok: true, message });
@@ -16,10 +13,32 @@ router.get('/bmi/:kg', (_, res)=>{
 
 });
 
-
 router.get('/bmi/:kg/:height', (req, res)=>{
-   
-    
+    const { kg } = req.params;
+    const { height } = req.params;
+    if(Number(kg) && Number(height))
+    {
+        var mass = height/100;
+        var h2 = mass * mass;
+        var bmi = kg/h2;
+        var f_bmi = Math.floor(bmi);
+        var diff  = bmi - f_bmi;
+        diff = diff * 10;
+        diff = Math.round(diff);
+        if (diff == 10){
+            f_bmi += 1;
+            diff = 0;
+        }
+        bmi = f_bmi + "." + diff;
+        bmi = bmi+"kg/m2";
+
+        const bmiCalc = [{ status : 'success', bmi: bmi }];
+        res.json({ ok: true, bmiCalc });
+    }
+    else{
+        const message = [{ status : 'error', error: 'Body Mass Index can\'t calculate on string values.' }]
+        res.json({ ok: true, message });
+    }
 });
 
 module.exports = router;
